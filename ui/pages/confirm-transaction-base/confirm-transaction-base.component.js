2b744c90ddd50c4f79c713f024ce05b20344e3f6
David Walsh <davidwalsh83@gmail.com>
2022-08-26 15:00:01 -0500

    Use async/await inside components

--- ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
+++ ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
@@ -862,7 +862,7 @@ export default class ConfirmTransactionBase extends Component {
     });
   }
 
-  handleCancel() {
+  async handleCancel() {
     const {
       txData,
       cancelTransaction,
@@ -874,10 +874,9 @@ export default class ConfirmTransactionBase extends Component {
 
     this._removeBeforeUnload();
     updateCustomNonce('');
-    cancelTransaction(txData).then(() => {
-      clearConfirmTransaction();
-      history.push(mostRecentOverviewPage);
-    });
+    await cancelTransaction(txData);
+    clearConfirmTransaction();
+    history.push(mostRecentOverviewPage);
   }
 
   handleSubmit() {
