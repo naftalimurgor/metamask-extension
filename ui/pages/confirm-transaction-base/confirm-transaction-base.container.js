0cd19e55f525870bad7ba7754198975bb4c859d9
amerkadicE <amer.kadic@endava.com>
2022-11-02 08:55:12 +0100

    Fix reverse array
    Fix unit test
    
    Fix recipient sort in Goerli network
    
    Move addToAddressBook logic from send screen to confirmation screen

--- ui/pages/confirm-transaction-base/confirm-transaction-base.container.js
+++ ui/pages/confirm-transaction-base/confirm-transaction-base.container.js
@@ -13,6 +13,7 @@ import {
   getNextNonce,
   tryReverseResolveAddress,
   setDefaultHomeActiveTabName,
+  addToAddressBook,
 } from '../../store/actions';
 import { isBalanceSufficient } from '../send/send.utils';
 import { shortenAddress, valuesFor } from '../../helpers/utils/util';
@@ -46,7 +47,9 @@ import {
   updateGasFees,
   getIsGasEstimatesLoading,
   getNativeCurrency,
+  getSendToAccounts,
 } from '../../ducks/metamask/metamask';
+import { addHexPrefix } from '../../../app/scripts/lib/util';
 
 import {
   parseStandardTokenTransactionData,
@@ -75,6 +78,14 @@ const customNonceMerge = (txData) =>
         customNonceValue,
       }
     : txData;
+
+function addressIsNew(toAccounts, newAddress) {
+  const newAddressNormalized = newAddress.toLowerCase();
+  const foundMatching = toAccounts.some(
+    ({ address }) => address.toLowerCase() === newAddressNormalized,
+  );
+  return !foundMatching;
+}
 
 const mapStateToProps = (state, ownProps) => {
   const {
@@ -123,6 +134,8 @@ const mapStateToProps = (state, ownProps) => {
   if (type !== TRANSACTION_TYPES.SIMPLE_SEND) {
     toAddress = propsToAddress || tokenToAddress || txParamsToAddress;
   }
+
+  const toAccounts = getSendToAccounts(state);
 
   const tokenList = getTokenList(state);
 
@@ -202,6 +215,7 @@ const mapStateToProps = (state, ownProps) => {
     balance,
     fromAddress,
     fromName,
+    toAccounts,
     toAddress,
     toEns,
     toName,
@@ -285,6 +299,13 @@ export const mapDispatchToProps = (dispatch) => {
       dispatch(setDefaultHomeActiveTabName(tabName)),
     updateTransactionGasFees: (gasFees) => {
       dispatch(updateGasFees({ ...gasFees, expectHexWei: true }));
+    },
+    showBuyModal: () => dispatch(showModal({ name: 'DEPOSIT_ETHER' })),
+    addToAddressBookIfNew: (newAddress, toAccounts, nickname = '') => {
+      const hexPrefixedAddress = addHexPrefix(newAddress);
+      if (addressIsNew(toAccounts, hexPrefixedAddress)) {
+        dispatch(addToAddressBook(hexPrefixedAddress, nickname));
+      }
     },
   };
 };
